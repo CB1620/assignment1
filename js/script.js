c8ac77b35d923d899872834dc962b64d19935baa
Christian <sofhsfosuhf@gmail.com>
2021-04-07 21:21:14 -0700

    added js blueprints

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// 1st function: dark theme
+//     when user clicks dark theme button,
+//         body background change to darker color
+//         text area change to dark color
+//         buttons change to dark color
+
+// 2nd function: light (original) theme
+//     when user clicks light theme button,
+//         body, buttons, textarea return to original colors
+
+// 3rd function: cancel
+//     make textarea dissappear
+//     make save, cancel buttons dissappear (target with class and just delete from page?)
+
+// 4th function: new note
+//     if "cancel" changes are in effect,
+//     new note button pressed =
+//     revert cancel changes
+
