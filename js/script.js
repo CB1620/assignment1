c4fc01bbb3fa27707e7576bd8c25ab62b9e27ed7
Christian <sofhsfosuhf@gmail.com>
2021-04-14 22:27:22 -0700

    added note saving/appending

--- js/script.js
+++ js/script.js
@@ -1,55 +1,67 @@
-// 1st function: dark theme
-//     when user clicks dark theme button,
-//         body background change to darker color
-//         text area change to dark color
-//         buttons change to dark color
-
 const theme = document.querySelectorAll("#noteBtn, #cancelBtn, #darkBtn, #saveBtn, #textarea, #sideBar")
 const cancel = document.querySelectorAll("#cancelBtn, #saveBtn, #textarea")
+const reverse = document.querySelectorAll("#cancelBtn, #saveBtn, #textarea")
+const notesArray = [{
+    title:"note one", 
+    body:"this is my first note",
+}]
+
 
 function toggle()
 {
     for (let id of theme){
         id.classList.toggle("changed");
     };
-    
+
 };
 
 function changeText() {
     var x = document.getElementById("darkBtn");
     if (x.innerHTML === "Light Theme") {
-      x.innerHTML = "Dark Theme";
-    } else {
-      x.innerHTML = "Light Theme";
-    }
-  }
+        x.innerHTML = "Dark Theme";
+        } else {
+            x.innerHTML = "Light Theme";
+        }
+}
 
   function remove()
-  {
-      for (let id of cancel){
-          id.classList.toggle("removed");
-      };
-      
-  };
+{
+    for (let id of cancel){
+        id.classList.toggle("removed");
+    };
+
+};
+
+  function reappear()
+{
+    for (let id of reverse){
+        id.classList.toggle("reappear");
+    };
+
+};
+
+function savePrompt()
+{
+    let title = prompt("What do you want to call this note?");
+    let body = document.getElementById("textarea").value;
+    notesArray.push({
+        title, body
+    })
+    list(title);
+};
+
+function list(header)
+{
+    let li = document.createElement('LI');
+    let note = document.createTextNode(header);
+    li.appendChild(note);
+    document.getElementById("notes").appendChild(li);
+
+}
 
 
 darkBtn.addEventListener("click", toggle);
 cancelBtn.addEventListener("click", remove);
+noteBtn.addEventListener("click", reappear);
 
-
-
-
-
-// 2nd function: light (original) theme
-//     when user clicks light theme button,
-//         body, buttons, textarea return to original colors
-
-// 3rd function: cancel
-//     make textarea dissappear
-//     make save, cancel buttons dissappear (target with class and just delete from page?)
-
-// 4th function: new note
-//     if "cancel" changes are in effect,
-//     new note button pressed =
-//     revert cancel changes
 
