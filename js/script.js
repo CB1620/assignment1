1b9e9394c65f983990a816b4e82bf4e900e47ba8
Christian <sofhsfosuhf@gmail.com>
2021-04-14 18:31:28 -0700

    added dark theme toggle js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,32 @@
 //         text area change to dark color
 //         buttons change to dark color
 
+const theme = document.querySelectorAll("#noteBtn, #cancelBtn, #darkBtn, #saveBtn, #textarea, #sideBar")
+const btn = document.querySelector("#darkBtn")
+
+function toggle()
+{
+    for (let id of theme){
+        id.classList.toggle("changed");
+    };
+    
+};
+
+function myFunction() {
+    var x = document.getElementById("darkBtn");
+    if (x.innerHTML === "Light Theme") {
+      x.innerHTML = "Dark Theme";
+    } else {
+      x.innerHTML = "Light Theme";
+    }
+  }
+
+darkBtn.addEventListener("click", toggle);
+
+
+
+
+
 // 2nd function: light (original) theme
 //     when user clicks light theme button,
 //         body, buttons, textarea return to original colors
